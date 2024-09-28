5e4328a1ec2c8b199904644af7a7ff6dcbc0123c
misha-fedyna <mihajlofedina5@gmail.com>
2024-09-28 15:41:22 +0300

    first master commit

---
+++ Lab5.3.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+double h(const double x);
+int main()
+{
+	double gp, gk, z;
+	int n;
+	cout << "gp = "; cin >> gp;
+	cout << "gk = "; cin >> gk;
+	cout << "n = "; cin >> n;
+
+	cout << "----------------------" << endl;
+	cout << "|" << setw(5) << "g" << "   |"
+		<< setw(10) << "z   " << " |"
+		<< endl;
+	cout << "----------------------" << endl;
+
+
+	double dg = (gk - gp) / n;
+	double g = gp;
+	while (g <= gk)
+	{
+		z = h(g * g + 1) + h(g + h(1) + 1);
+		
+		cout << "|" << setw(7) << setprecision(2) << g << " |"
+			<< setw(10) << setprecision(5) << z << " |"
+			<< endl;
+		cout << "----------------------" << endl;
+		g += dg;
+	}
+	return 0;
+}
+double h(const double x)
+{
+	if (abs(x) >= 1)
+		return (sin(x) + 1) / (1 + cos(x));
+	else
+	{
+		double S = 0;
+		int i = 1;
+		double a = 1;
+		S = a;
+		do
+		{
+			i++;
+			double R = pow(x, i) / ((2 * i - 2) * (2 * i - 1) * 2 * i);
+			a *= R;
+			S += a;
+		} while (i < 5);
+		return S;
+	}
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double m, res;
+			m = h(2.0);
+			res = (sin(2.0) + 1) / (1 + cos(2.0));
+			Assert::AreEqual(m, res, 0.000001);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			double expected_value = 1.01043;
+			double actual_value = h(0.5);
+			Assert::AreEqual(expected_value, actual_value, 0.1);  // Допуск можна зменшити
+		}
+	};
+}
